d527053435468e04323eb99bf89819ef16efd000
juanitotaveras <juanito.taveras@gmail.com>
2015-10-28 18:18:24 -0500

    I wrote a little Python script that replaces the Major Topic number with the correct topic. See if maybe you can do this in R! But now it should be a little easier to work with the data.

---
+++ fixer.py
@@ -0,0 +1,27 @@
+
+def main():
+	inFile = open ("./Gallups_Most_Important_Problem.csv", "r")
+	outFile = open ("./Gallups_Most_Important_Problem_reformatted.csv", "w")
+	topics = {1:"Macroeconomics", 2:"Civil Rights", 3:"Health", 4:"Agriculture", 5:"Labor", 6:"Education", 7:"Environment", 8:"Energy", 10:"Transportation", 12:"Crime", 13:"Social Welfare", 14:"Housing & Development", 15:"Domestic Commerce", 16:"Defernse", 17:"Science & Technology", 18:"Foreign Trade", 19:"International Affairs", 20:"Government Operations", 21:"Public Lands", 25:"Don't Know/Other"}
+	
+	for line in inFile:
+		print(line)
+		line2 = line.split(",")
+		newline = ""
+		if ((line2[3]) != "MajorTopic") and (int(line2[3]) in topics):
+			if int(line2[3]) in topics:
+				top = topics[int(line2[3])]
+				newline = line2[0] + "," + line2[1] + "," + line2[2] + "," + top + "," + line2[4]
+			else:
+				newline = line2[0] + "," + line2[1] + "," + line2[2] + "," + "N/A" + "," + line2[4]			 
+		else:
+			newline = line
+		print (newline)
+		outFile.write(newline)
+
+
+
+	inFile.close()
+	outFile.close()
+	
+main()
