c9adad1fda78f8100f3c8a97456da3eda4d2b124
Zareen Khayrattee <zareenk94@yahoo.com>
2015-11-13 17:24:26 -0600

    added my SQL bar chart to RMD and knitted html... bar labels won't work, but everything else is good. will keep trying to get labels in

--- 03 R SQL Visualizations/Bar3.R
+++ 03 R SQL Visualizations/Bar3.R
@@ -8,7 +8,7 @@ require("extrafont")
 
 df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select majortopic, year, sum(sum_percentage) as sum_percentage, avg_percentage, avg(avg_percentage) OVER (PARTITION BY majortopic) as window_avg from (select year, majortopic, sum(PERCENTAGE) as sum_percentage, avg(percentage) as avg_percentage from GALLUPS group by year, majortopic) where ((year="1990" or year="1995" or year="2000" or year="2005" or year="2010") and (majortopic!=\'Agriculture\' and majortopic!=\'Domestic Commerce\' and majortopic!=\'Housing  and  Development\' and majortopic!=\'Labor\' and majortopic!=\'Public Lands\' and majortopic!=\'Transportation\' and (majortopic=\'Immigration\' or majortopic=\'International Affairs\' or majortopic=\'Macroeconomics\' or majortopic=\'Science  and  Technology\' or majortopic=\'Social Welfare\'))) group by year, majortopic, avg_percentage order by majortopic, year "'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_kdk745', PASS='orcl_kdk745', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
 
-spread(df, YEAR, SUM_PERCENTAGE) %>% View
+spread(df, YEAR, SUM_PERCENTAGE)
 
 ggplot() + 
   coord_cartesian() + 
@@ -18,23 +18,23 @@ ggplot() +
   labs(title='Major Topic Public Opinion Poll Barchart') +
   labs(x=paste("YEAR"), y=paste("SUM_PERCENTAGE")) +
   layer(data=df, 
-        mapping=aes(x=factor(YEAR), y=SUM_PERCENTAGE), 
+        mapping=aes(x=factor(YEAR), y=SUM_PERCENTAGE, fill=SUM_PERCENTAGE), 
         stat="identity", 
         stat_params=list(), 
         geom="bar",
         geom_params=list(colour="blue"), 
         position=position_identity()
-  ) + coord_flip() + 
-  layer(data=df, 
-        mapping=aes(x=factor(YEAR), y=SUM_PERCENTAGE, label=round(SUM_PERCENTAGE)), 
-        stat="identity", 
-        stat_params=list(), 
-        geom="text",
-        geom_params=list(colour="black", hjust=-0.5), 
-        position=position_identity()
-  ) +
+  ) + coord_flip() +
   layer(data=df, 
         mapping=aes(yintercept = WINDOW_AVG), 
         geom="hline",
         geom_params=list(colour="red")
-  ) 
+  ) #+ 
+  #layer(data=df, 
+        #mapping=aes(x=factor(YEAR), y=SUM_PERCENTAGE, label=round(SUM_PERCENTAGE)), 
+        #stat="identity", 
+        #stat_params=list(), 
+        #geom="text",
+        #geom_params=list(colour="black", hjust=-0.5), 
+        #position=position_identity()
+  #) 
