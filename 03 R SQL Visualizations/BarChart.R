69ab862e64930df1aca590438b0e35861e108c1c
Zareen Khayrattee <zareenk94@yahoo.com>
2015-11-13 19:45:47 -0600

    added labels to my bar graphs

--- 03 R SQL Visualizations/BarChart.R
+++ 03 R SQL Visualizations/BarChart.R
@@ -1,3 +1,7 @@
+require("jsonlite")
+require("RCurl")
+require("ggplot2")
+require("dplyr")
 
 df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select majortopic, year, sum(sum_percentage) as sum_percentage, avg_percentage, avg(avg_percentage) OVER (PARTITION BY majortopic) as window_avg from (select year, majortopic, sum(PERCENTAGE) as sum_percentage, avg(percentage) as avg_percentage from GALLUPS group by year, majortopic) where ((year="1990" or year="1995" or year="2000" or year="2005" or year="2010") and (majortopic!=\'Agriculture\' and majortopic!=\'Domestic Commerce\' and majortopic!=\'Housing  and  Development\' and majortopic!=\'Labor\' and majortopic!=\'Public Lands\' and majortopic!=\'Transportation\')) group by year, majortopic, avg_percentage order by majortopic, year "'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_kdk745', PASS='orcl_kdk745', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
 
@@ -22,20 +26,22 @@ ggplot() +
         mapping=aes(yintercept = WINDOW_AVG), 
         geom="hline",
         geom_params=list(colour="red")
-  ) #+ 
-#layer(data=df, 
-#mapping=aes(x=factor(YEAR), y=SUM_PERCENTAGE, label=round(SUM_PERCENTAGE)), 
-#stat="identity", 
-#stat_params=list(), 
-#geom="text",
-#geom_params=list(colour="black", hjust=-0.5), 
-#position=position_identity()
-#) +
-#layer(data=df, 
-#mapping=aes(x=factor(YEAR), y=AVG_PERCENTAGE, label=round(AVG_PERCENTAGE - WINDOW_AVG)), 
-#stat="identity", 
-#stat_params=list(), 
-#geom="text",
-#geom_params=list(colour="black", hjust=-5), 
-#position=position_identity()
-#) 
+  ) + 
+layer(data=df, 
+mapping=aes(x=factor(YEAR), y=SUM_PERCENTAGE, label=sprintf("%2.4f",SUM_PERCENTAGE)), 
+stat="identity", 
+stat_params=list(), 
+geom="text",
+label=value,
+geom_params=list(colour="black", hjust=-0.5), 
+position=position_identity()
+) +
+layer(data=df, 
+mapping=aes(x=factor(YEAR), y=AVG_PERCENTAGE, label=sprintf("%2.4f",WINDOW_AVG)), 
+stat="identity", 
+stat_params=list(), 
+geom="text",
+label=value,
+geom_params=list(colour="red", hjust=-5), 
+position=position_identity()
+) 
