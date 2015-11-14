c832b1b8f3d2194b61e8f7b10cb7ad0dea12fbf8
Juanito Taveras <juanito.taveras@gmail.com>
2015-11-13 18:11:53 -0600

    Modified my Tableau scatterplot and added it to the .rmd

--- 03 R SQL Visualizations/Scatterplot.R
+++ 03 R SQL Visualizations/Scatterplot.R
@@ -2,16 +2,10 @@ require("jsonlite")
 require("RCurl")
 require("ggplot2")
 require("extrafont")
-# Change the USER and PASS below to be your UTEid
-df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="SELECT Year, Majortopic, Congress, Percentage FROM GALLUPS WHERE ((MAJORTOPIC=\\\'Civil Rights\\\' OR MAJORTOPIC=\\\'Crime\\\' OR MAJORTOPIC=\\\'Education\\\' OR MAJORTOPIC = \\\'Health\\\' OR MAJORTOPIC=\\\'Social Welfare\\\') AND (YEAR > 1960)) ORDER BY YEAR;"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_kdk745', PASS='orcl_kdk745', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-#df
-#names(df)
-#summary(df)
-#head(df)
-#summary(df)
-#head(df)
 
-#View(df)
+df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="SELECT Year, Majortopic, Congress, Percentage FROM GALLUPS WHERE ((MAJORTOPIC=\\\'Civil Rights\\\' OR MAJORTOPIC=\\\'Crime\\\' OR MAJORTOPIC=\\\'Education\\\' OR MAJORTOPIC = \\\'Health\\\' OR MAJORTOPIC=\\\'Social Welfare\\\') AND (YEAR > 1960)) ORDER BY YEAR;"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_kdk745', PASS='orcl_kdk745', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+
 ggplot() + 
   coord_cartesian() + 
   scale_x_continuous() +
